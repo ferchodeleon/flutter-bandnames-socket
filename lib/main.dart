dafd5f88885d0a4d829d347a96ccd8ee3786a8ba
Fernando De Leon <gasnare@hotmail.com>
2022-09-05 09:35:48 -0500

    connect with backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:band_name/pages/status.dart';
+import 'package:band_name/services/socket_service.dart';
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
 
 import 'package:band_name/pages/home.dart';
 
@@ -8,14 +11,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'status',
-      routes: {
-        'home': (_) => HomePage(),
-        'status': (_) => StatusPage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SocketService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'status',
+        routes: {
+          'home': (_) => HomePage(),
+          'status': (_) => StatusPage(),
+        },
+      ),
     );
   }
 }
--- lib/pages/status.dart
+++ lib/pages/status.dart
@@ -1,10 +1,14 @@
+import 'package:band_name/services/socket_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class StatusPage extends StatelessWidget {
   const StatusPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final socketService = Provider.of<SocketService>(context);
+
     return Scaffold(
       body: Container(
         child: Center(
