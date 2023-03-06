ec9bd96ad1ae995e56592c06af0f7bdca4c4bdd2
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-06 14:38:24 -0500

    add validation in case not structure information desired

--- lib/model/band.dart
+++ lib/model/band.dart
@@ -10,8 +10,8 @@ class Band {
   });
 
   factory Band.fromMap(Map<String, dynamic> object) => Band(
-        id: object['id'],
-        name: object['name'],
-        votes: object['votes'],
+        id: object.containsKey('id') ? object['id'] : 'no-id',
+        name: object.containsKey('name') ? object['name'] : 'no-name',
+        votes: object.containsKey('votes') ? object['votes'] : 'no-votes',
       );
 }
