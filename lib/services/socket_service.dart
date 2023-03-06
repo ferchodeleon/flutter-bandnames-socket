e35be5fcf0213114c28e647178c98855f72425db
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-06 14:46:10 -0500

    change ip banckend

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -20,7 +20,7 @@ class SocketService with ChangeNotifier {
 
   void _initConfig() {
     // Dart client
-    _socket = IO.io('http://192.168.1.43:3000', {
+    _socket = IO.io('http://192.168.1.53:3000', {
       'transports': ['websocket'],
       'autoConnect': true,
     });
